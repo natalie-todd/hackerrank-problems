0a5b3ad0012b3e6ad0c60b02bff0490786645675
natalie-todd <natalie.todd15@gmail.com>
2019-10-29 20:42:27 -0600

    Brute force solution

--- src/main/java/TwoDArrayDS.java
+++ src/main/java/TwoDArrayDS.java
@@ -1,10 +1,8 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+//1
 public class TwoDArrayDS {
-    // Complete the hourglassSum function below.
 
     public static int[][] sampleInput = {
             {1, 1, 1 ,0, 0, 0},
@@ -16,8 +14,36 @@ public class TwoDArrayDS {
     };
 
     static int hourglassSum(int[][] arr) {
-        return 8;
+        List<Integer> listOfTotals = new ArrayList<>();
 
+        for(int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                try {
+                    int one = arr[i][j];
+                    int two = arr[i][j + 1];
+                    int three = arr[i][j + 2];
+                    int middle = arr[i + 1][j + 1];
+                    int four = arr[i + 2][j];
+                    int five = arr[i + 2][j + 1];
+                    int six = arr[i + 2][j + 2];
+                    int iTotal = one + two + three + middle + four + five + six;
+                    listOfTotals.add(iTotal);
+                } catch(ArrayIndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+        }
+        Collections.sort(listOfTotals);
+        return listOfTotals.get(listOfTotals.size() -1);
+    }
+
+    public static int[][] sout(int[][] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr[i].length -1; j++) {
+                System.out.println(arr[i][j] + " is at index " + i+j);
+            }
+        }
+        return arr;
     }
 
 
